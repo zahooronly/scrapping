39236162ce5158e4dcbbc259bc0d0f16987b96be
ZahoorOnly <i.am.zahoor.a@outlook.com>
2024-10-19 21:06:07 +0500

    (basics): for loop and if condition

---
+++ basics/basics.py
@@ -0,0 +1,47 @@
+# python basics for learning web scrapping
+
+states=[
+    "Alabama",
+    "Alaska",
+    "Arizona",
+    "Arkansas",
+    "California",
+    "Colorado",
+    "Connecticut",
+    "Delaware",
+    "Florida",
+    "Georgia",
+    "Hawaii",
+    "Idaho",
+    "Illinois",
+    "Indiana",
+    "Iowa",
+    "Kansas",
+    "Kentucky",
+    "Louisiana",
+    "Maine",
+    "Maryland",
+    "Massachusetts",
+    "Michigan",
+    "Minnesota",
+    "Mississippi",
+    "Missouri",
+    "Montana",
+    "Nebraska",
+    "Nevada",
+    "New Hampshire",
+    "New Jersey",
+    "New Mexico",
+    "New York",
+    "North Carolina",
+]
+
+# print(states[1])
+
+# loops
+
+for state in states:
+    # print(state)
+    # conditions
+    if state=="Florida":
+        print(state)
